e35bf705a4432a47d5de90973f1d79ccbcfb6407
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-09 23:42:03 -0600

    for in vs for of

--- Exercise Files/Chp_02/02_05/app.ts
+++ Exercise Files/Chp_02/02_05/app.ts
@@ -4,7 +4,13 @@ var array = [
     "Save Gotham"
 ];
 
-for (var value of array) {
+for (var value in array) { //in 
     var content = array[value];  //have to call the content in order to actually get it out of the array 
+    console.log(content);
+}
+
+//vs for of
+
+for (var value of array) { //in 
     console.log(content);
 }
