b8d621465c63dbff77899179ecfd91e3a5939a04
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-09 23:26:56 -0600

    const should not be used within block for changing varibles

--- Exercise Files/Chp_02/02_04/app.ts
+++ Exercise Files/Chp_02/02_04/app.ts
@@ -1,10 +1,10 @@
 var container = document.getElementById('container');
 //why is var vs let 
-for (let x = 0; x <= 5; x++) {
-    const counter = x;
+for (var x = 0; x <= 5; x++) {
+    let counter = x; //should be let, const does not work since it's been initialized 
     counter = 1;
     //can't find when using let?
-    //related to block scope 
+    //related to block scope
 }
 
 console.log(counter);
