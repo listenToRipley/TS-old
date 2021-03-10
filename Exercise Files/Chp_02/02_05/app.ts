c4984e99678dce190ef93bca4075241deb17fbd1
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-09 23:28:44 -0600

    extracting content from an array

--- Exercise Files/Chp_02/02_05/app.ts
+++ Exercise Files/Chp_02/02_05/app.ts
@@ -5,5 +5,6 @@ var array = [
 ];
 
 for (var value of array) {
-    console.log(value);
+    var content = array[value];  //have to call the content in order to actually get it out of the array 
+    console.log(content);
 }
