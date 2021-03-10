fb2abb40643f9197768088f9cb7c98c7a2a40888
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-09 23:25:33 -0600

    error with let vs var in loop?"

--- Exercise Files/Chp_02/02_04/app.ts
+++ Exercise Files/Chp_02/02_04/app.ts
@@ -1,8 +1,10 @@
 var container = document.getElementById('container');
-
-for (var x = 0; x <= 5; x++) {
+//why is var vs let 
+for (let x = 0; x <= 5; x++) {
     const counter = x;
     counter = 1;
+    //can't find when using let?
+    //related to block scope 
 }
 
 console.log(counter);
